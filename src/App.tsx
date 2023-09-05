9a9b6f2fcbe7cec55d1113c818a92bb02f90d45b
Rodrigo Wahl <wahl@artech-is.com>
2023-09-05 14:12:55 +0200

    feat:set up base app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
+import { HomePage } from './pages/HomePage/HomePage';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { staleTime: 10 * 60 * 100, cacheTime: 200000 },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Edit and save to reload.</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <HomePage />
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }
 

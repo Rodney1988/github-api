90fed23de45d4884b2857d676b7ce7e57c858e0a
Rodrigo Wahl <wahl@artech-is.com>
2023-10-12 11:57:48 +0200

    feat: add prod url

--- src/urls.ts
+++ src/urls.ts
@@ -4,7 +4,7 @@ let baseUrl = '';
 if (env === 'development') {
   baseUrl = 'http://localhost:5001/github-api-317c6/us-central1/nodeApi';
 } else if (env === 'production') {
-  baseUrl = '';
+  baseUrl = 'https://nodeapi-al3vvliyoq-uc.a.run.app';
 }
 
 export { baseUrl };
